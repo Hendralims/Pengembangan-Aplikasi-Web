d366844a256bcb0dc3f8d249a6030cce5b8db11b
Hendralims <192112925@students.mikroskil.ac.id>
2021-11-20 01:42:57 +0700

    commit on 'Tugas 6'

--- latihan/src/App.js
+++ latihan/src/App.js
@@ -1,109 +1,141 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import {
   BrowserRouter as Router,
   Route,
-  Link, 
   Switch,
   Redirect,
   withRouter,
-  NavLink
-
+  NavLink,
 } from "react-router-dom";
+import "./css/bootstrap.min.css";
+import Foto from "./Foto.JPG";
 
-
-class App extends Component{
-
-
-  constructor(){
-    super()
-    this.myRef = React.createRef();
-    
-    }
-    
-
-
-  componentDidMount(){
-    this.myRef.current.style.height = '100vh';
-    this.myRef.current.style.width = '100%';
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { isAuth: false };
   }
 
-  setBackground(bgColor, txtColor){
-    this.myRef.current.style.background = bgColor;
-    this.myRef.current.style.color = txtColor;
-  }
+  render() {
+    const LoginButton = withRouter(({ history }) => (
+      <button
+        onClick={() => {
+          this.setState({ isAuth: true });
+          history.push("/profile");
+        }}
+      >
+        Login
+      </button>
+    ));
 
-  render(){
-    const LoginButton = withRouter(({history})=>(
-      <button onClick={()=>{this.setState({isAuth: true}) 
-      history.push('/profile')}}>Login</button>
-    ))
+    const LogoutButton = withRouter(({ history }) => (
+      <button
+        onClick={() => {
+          this.setState({ isAuth: false });
+          history.push("/login");
+        }}
+      >
+        Logout
+      </button>
+    ));
 
-    const LogoutButton = withRouter(({history})=>(
-      <button onClick={()=>{this.setState({isAuth: false}) 
-      history.push('/login')}}>Logout</button>
-    ))
-    
-    const routes = [{
-      path : '/',
-      exact : true,
-      render : () => <div>Ini Halaman Home</div>
-    },{
-      path : '/news',
-      render : () => <div>Ini Halmaan News</div>
-    },{
-      path : '/login',
-      render : () => <LoginButton />
-    },{
-        path : '/profile',
-        render : () => this.state.isAuth ? <div>Ini Halaman Profile <br/><LogoutButton /></div> :<Redirect to='/login' />
-    }]
+    const routes = [
+      {
+        path: "/",
+        exact: true,
+        render: () => <div>Ini Halaman Home</div>,
+      },
+      {
+        path: "/login",
+        render: () => <LoginButton />,
+      },
+      {
+        path: "/profile",
+        render: () =>
+          this.state.isAuth ? (
+            <div>
+              <div className="container text-center">
+                <h1>Profile Mahasiswa</h1>
+                <table className="table table-borderless text-start">
+                  <tbody>
+                    <tr>
+                      <td>Foto Profil</td>
+                      <td>:</td>
+                      <td>
+                        <img src={Foto} className="w-25 h-5 border border-1" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Nama</td>
+                      <td>:</td>
+                      <td>Hendra</td>
+                    </tr>
+                    <tr>
+                      <td>NIM</td>
+                      <td>:</td>
+                      <td>192112925</td>
+                    </tr>
+                    <tr>
+                      <td>Jenis Kelamin</td>
+                      <td>:</td>
+                      <td>Laki-Laki</td>
+                    </tr>
+                    <tr>
+                      <td>Tempat/Tanggal Lahir</td>
+                      <td>:</td>
+                      <td>Medan, 14 April 2001</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <LogoutButton />
+                      </td>
+                      <td></td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+                <br />
+              </div>
+            </div>
+          ) : (
+            <Redirect to="/login" />
+          ),
+      },
+    ];
 
-    return(
+    return (
       <div>
-          
-      <Router>
-        <div>
-          <ul style={{listStyle: 'none'}}>
-            <li><NavLink exact activeStyle={{fontWeight: 'bold', color:'red'}} to='/'>Home</NavLink></li>
-            <li><NavLink activeclassName='active-link' to='/news'>News</NavLink></li>
-            <li><NavLink activeclassName='active-link' to='/profile'>Profile</NavLink></li>
-          </ul>
-        
+        <Router>
+          <div>
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink exact className="nav-link" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink exact className="nav-link" to="/profile">
+                    Profile
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
 
           <Switch>
-            {
-              routes.map((item, index)=>(
-                <Route path={item.path} exact={item.exact} render={item.render}/>
-              ))
-            }
+            {routes.map((item, index) => (
+              <Route path={item.path} exact={item.exact} render={item.render} />
+            ))}
           </Switch>
-        </div>
-      </Router>
-
-      <div ref={this.myRef}>
-            <p>Belajar Pengembangan Aplikasi Web dengan ReactJS dengan Topik Router dan Refs</p>
-            <button onClick={this.setBackground.bind(this, 'white', 'black')}>WHITE</button>
-            <button onClick={this.setBackground.bind(this, 'red', 'white')}>RED</button>
-            <button onClick={this.setBackground.bind(this, 'green', 'white')}>GREEN</button>
-            <button onClick={this.setBackground.bind(this, 'blue', 'white')}>BLUE</button>
-      
+        </Router>
       </div>
-      
-      </div>
-
-
-    )
+    );
   }
-  
 }
 
 export default App;
-
-
-
-
-
 
 // Pertemuan 5
 
@@ -134,7 +166,7 @@ export default App;
 //       else if(this.state.view == 'help')
 //       return <Help />
 //     }
-    
+
 //     return(
 //       <div>
 //         <Header getValue={(value)=>this.setState({view:value})} />
